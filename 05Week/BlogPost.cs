6205d802ffb44787e8bf74d348b796e3b1d9fe38
Maxfield Gaj <maxfieldegaj@gmail.com>
2017-11-07 20:43:00 -0600

    Displayed private fields in blog project

--- 05Week/BlogPost.cs
+++ 05Week/BlogPost.cs
@@ -3,7 +3,81 @@ using System;
 public class Program
 {
 	public static void Main()
-	{
-		Console.WriteLine("Hello World");
-	}
+    {
+        BlogPost myBlog = new BlogPost("I am learning C#");
+        myBlog.Description = "This is the content of the blog";
+        myBlog.Created = DateTime.Now;
+
+        // voting
+        myBlog.IncrementDownvote();
+        myBlog.IncrementUpvote();
+        myBlog.IncrementUpvote();
+
+        Console.WriteLine(myBlog.Title);
+        Console.WriteLine(myBlog.Description);
+        Console.WriteLine(myBlog.CreatedOn);
+
+        Console.WriteLine(myBlog.Upvote);
+        Console.WriteLine(myBlog.Downvote);
+
+
+
+        Console.ReadLine();
+    }
+}
+
+public class BlogPost
+{
+    //Field
+    public string Title;
+    public string Description;
+    public DateTime Created;
+
+    // Non public field
+    // avoid having them set to zero
+    private int _upvote;
+    private int _downvote;
+
+    // Constructor   // BlogPost myBlog = new BlogPost();
+    public BlogPost()
+    {
+        _downvote = 0;
+        _upvote = 0;
+    }
+
+    //Constructor OVerload
+    public BlogPost(string title)
+    {
+        Title = title;
+    }
+
+    // Properties
+    public string Upvote {
+        get {
+            return _upvote + " Upvotes";
+        }
+    }
+    public string Downvote {
+        get {
+            return _downvote + " Downvotes";
+        }
+
+    }
+
+    public string CreatedOn {
+        get
+        {
+            return "Created On " + string.Format("{0:MM/dd/yy}", Created);
+        }
+    }
+    // Methods
+    public void IncrementUpvote()
+    {
+        _upvote++;
+    }
+
+    public void IncrementDownvote()
+    {
+        _downvote++;
+    }
 }
