87929f66906bb300d6ddd349d4b2f4f11982e00b
Maxfield Gaj <maxfieldegaj@gmail.com>
2017-11-09 21:07:23 -0600

    Began CHeckers. Error on Draw Board

--- 05Week/Checkers.cs
+++ 05Week/Checkers.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 public class Program
 {
     public static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
+        Game game = new Game();
+        Console.ReadLine();
         // Your code here
     }
 }
@@ -16,17 +20,58 @@ public class Checker
     public int[] Position  { get; set; }
     public string Color { get; set; }
     
+    // Constructor
     public Checker(string color, int[] position)
     {
-        // Your code here
+        Color = color;
+        Position = position;
+
+        //  Use unicode to represent the symbol'
+        // when we print to the console
+        Symbol = color == "white" ? "\u25cf": "\u25cb";
     }
 }
 
 public class Board
 {
-    public string[][] Grid  { get; set; }
+    public string[][] Grid { get; set; }
     public List<Checker> Checkers { get; set; }
-    
+    // variable
+
+    // position of white checkers
+    int[][] whitePosition = new int[][]
+    {
+        new int[] { 0, 1 },
+        new int[] {0, 3 },
+        new int[] {0, 5 },
+        new int[] {0, 7 },
+        new int[] {1, 0 },
+        new int[] {1, 2 },
+        new int[] {1, 4 },
+        new int[] { 1, 6 },
+        new int[] {2, 1 },
+        new int[] {2, 3 },
+        new int[] {2, 5 },
+        new int[] {2, 7 },
+    };
+
+    // position of black checkers
+    int[][] blackPosition = new int[][]
+    {
+       new int[] { 5, 0 },
+       new int[] {5, 2 },
+       new int[] {5, 4 },
+       new int[] {5, 6 },
+       new int[] {6, 1 },
+       new int[] {6, 3},
+       new int[] {6, 5},
+       new int[] {6, 7},
+       new int[] {7, 0},
+       new int[] {7, 2},
+       new int[] {7, 4},
+       new int[] {7, 6}
+    };
+
     public Board()
     {
         // Your code here
@@ -35,24 +80,66 @@ public class Board
     
     public void CreateBoard()
     {
-        // Your code here
+        // Board is 8x8
+        Grid = new string[][]
+        {
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+            new string[] { " ", " ", " ", " ", " ", " ", " ", " "},
+        };
         return;
     }
     
     public void GenerateCheckers()
     {
-        // Your code here
+        // 12 pieces per color - 24 all together
+        // white position:
+        Checkers = new List<Checker>();
+        for (var i = 0; i < whitePosition.Length; i++)
+        {
+            Checker checker = new Checker("white", whitePosition[i]);
+            Checkers.Add(checker);
+        }
+        // black position:
+        for (var i = 0; i < blackPosition.Length; i++)
+        {
+            Checker checker = new Checker("black", blackPosition[i]);
+            Checkers.Add(checker);
+
+        }
+
         return;
     }
     
     public void PlaceCheckers()
     {
-        // Your code here
+        // CHeckers List (property) contains all 24 pieces 
+        foreach(var c in Checkers)
+        {
+            int[] position = c.Position; // ex: [0, 2]
+
+            Grid[position[0]][position[1]] = c.Symbol;
+        }
         return;
     }
     
     public void DrawBoard()
     {
+        Console.WriteLine("0  1  2  3  4  5  6  7");
+        for(int i = 0; i < Grid.GetLength(1); i++) // row
+        {
+            string column = $"{i} ";
+
+            for(int e = 0; e < Grid.GetLength(2); e++) // column
+            {
+                column += Grid[i][e];
+            }
+        }
         // Your code here
         return;
     }
@@ -78,6 +165,20 @@ class Game
 {
     public Game()
     {
-        // Your code here
+        Board board = new Board();
+        board.GenerateCheckers();
+        board.CreateBoard();
+        board.PlaceCheckers();
+
+        do
+        {
+            board.DrawBoard();
+            //make moves
+            //remove checker
+
+        } while ();
+
+
+        
     }
 }
